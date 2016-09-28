ed5965addf280ae855e01c86c7804835c04705f6
rachelsalazar <ret1992@gmail.com>
2016-09-28 12:56:03 -0700

    added form to html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var cookieTable = document.getElementById('cookietable');
+var cookieInputForm = document.getElementById('cookieinputform');
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var allStores = [];
